9d703942f6316eda82be8901d56a93e494094b5a
Sanketh Gunasekara <sanketh.gunasekara@1billiontech.com>
2023-10-17 06:06:42 +0530

    Updated the css

--- JointJSPCF/src/App.tsx
+++ JointJSPCF/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Rappid from './rappid';
 import './App.css'
+import "@clientio/rappid/rappid.css"
 
 class App extends React.Component {
     public render() {
